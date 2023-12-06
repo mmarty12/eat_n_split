0e2ed54f2edafb156efc1902dee558db7984441e
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-12-06 17:06:52 +0200

    fixed form state reseting with the key prop

--- src/App.js
+++ src/App.js
@@ -64,7 +64,11 @@ export default function App() {
         <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
       </div>
       {selectedFriend && (
-        <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
+        />
       )}
     </div>
   );
